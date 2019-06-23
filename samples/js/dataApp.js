b74322de6049b0e700d91c47c7361bd60f866bb7
M P Sunil <sunil.mp@tavant.com>
2019-06-23 20:31:19 +0530

    added pagination

--- samples/js/dataApp.js
+++ samples/js/dataApp.js
@@ -2,68 +2,92 @@
 
 /* App Module */
 
-var app = angular.module('webixApp', [ "webix" ]);
+var app = angular.module('webixApp', ['webix', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-app.controller("webixTestController", function($scope,$http){
-	
-  $scope.records = [];
-	var model = new Object();
-	model.pagination = new Object();;
-	model.pagination.currentPage = 1;
-	model.pagination.itemsPerPage = 300;
-	$http.post("http://localhost:5100/api/Employee/GetEmployees",model).then(
-      function successCallback(response) {
-				console.log("Result : "+JSON.stringify(response));
-        $scope.records = response;
+app.controller("webixTestController", function ($scope, $http) {
+
+	$scope.records = [];
+
+	$scope.totalItems = 60;
+	$scope.currentPage = 4;
+	$scope.itemPerPage = 5;
+
+	$scope.setPage = function (pageNo) {
+		$scope.currentPage = pageNo;
+	};
+
+	$scope.pageChanged = function () {
+		console.log('Page changed to: ' + $scope.currentPage);
+		$scope.pageLoad();
+	};
+	$scope.pageLoad = function () {
+		$http({
+			url: "http://localhost:5100/api/Employee/GetEmployees",
+			method: "GET",
+			params: { cur: $scope.currentPage, total: $scope.itemPerPage }
+		}).then(
+			function successCallback(response) {
+				console.log("Result : " + JSON.stringify(response));
+				$scope.records = response.data.employees;
+				$scope.totalItems = response.data.pagination.totalItems;
 			},
 		);
-  // 	{ id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
+	};
+
+	$scope.pageLoad();
+	// $http.get("http://localhost:5100/api/Employee/GetEmployees").then(
+	//     function successCallback(response) {
+	// 			console.log("Result : "+JSON.stringify(response));
+	//       $scope.records = response;
+	// 		},
+	// 	);
+	// 	{ id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
 	// { id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2},
 	// { id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3},
 	// { id:4, title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4},
 	// { id:5, title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5},
 	// { id:6, title:"12 Angry Men", year:1957, votes:164558, rating:8.9, rank:6}
-  // ];
+	// ];
 
-  $scope.lines = [
-  	{ id:1, sales:20, year:"02"},
-  	{ id:2, sales:55, year:"03"},
-  	{ id:3, sales:40, year:"04"},
-  	{ id:4, sales:78, year:"05"},
-  	{ id:5, sales:61, year:"06"},
-  	{ id:6, sales:35, year:"07"},
-  	{ id:7, sales:80, year:"08"},
-  	{ id:8, sales:50, year:"09"},
-  	{ id:9, sales:65, year:"10"},
-  	{ id:10, sales:59, year:"11"}
-  ];
+	$scope.lines = [
+		{ id: 1, sales: 20, year: "02" },
+		{ id: 2, sales: 55, year: "03" },
+		{ id: 3, sales: 40, year: "04" },
+		{ id: 4, sales: 78, year: "05" },
+		{ id: 5, sales: 61, year: "06" },
+		{ id: 6, sales: 35, year: "07" },
+		{ id: 7, sales: 80, year: "08" },
+		{ id: 8, sales: 50, year: "09" },
+		{ id: 9, sales: 65, year: "10" },
+		{ id: 10, sales: 59, year: "11" }
+	];
 
-  $scope.addRecord = function(){
-  	$scope.records.push({
-  		title:"New Record",
-  		rating:999,
-  		votes:0,
-  		year:2013
-  	});
-  };
+	$scope.addRecord = function () {
+		$scope.records.push({
+			title: "New Record",
+			rating: 999,
+			votes: 0,
+			year: 2013
+		});
+	};
 
-  $scope.changeLine = function(type){
-  	$$("mychart").define("type", type);
-  	$$("mychart").render();
-  };
+	$scope.changeLine = function (type) {
+		$$("mychart").define("type", type);
+		$$("mychart").render();
+	};
 
-  $scope.showDetails = function(id, details){
-  	$scope.selectedId = id.row;
-    $scope.eventType = details[1].type;
-    $scope.nativeElement = details[2].nodeName;
-    $scope.$digest();
-  };
+	$scope.showDetails = function (id, details) {
+		$scope.selectedId = id.row;
+		$scope.eventType = details[1].type;
+		$scope.nativeElement = details[2].nodeName;
+		$scope.$digest();
+	};
 
-  $scope.showHeaderDetails = function(id, details){
-    $scope.sortedCol = id;
-    $scope.sortDir = details[1];
-    $scope.sortType = details[2];
-    $scope.$digest();
-  };
+	$scope.showHeaderDetails = function (id, details) {
+		$scope.sortedCol = id;
+		$scope.sortDir = details[1];
+		$scope.sortType = details[2];
+		$scope.$digest();
+	};
 });
 
